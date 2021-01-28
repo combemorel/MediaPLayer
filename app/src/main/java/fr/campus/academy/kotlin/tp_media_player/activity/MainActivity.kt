e1f1746613ca2231bd7c4adbb2e9f6b12f220d47
combemorel <d.combemorel@orange.fr>
2021-01-28 12:05:00 +0100

    init commit project

---
+++ app/src/main/java/fr/campus/academy/kotlin/tp_media_player/activity/MainActivity.kt
@@ -0,0 +1,12 @@
+package fr.campus.academy.kotlin.tp_media_player.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import fr.campus.academy.kotlin.tp_media_player.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
