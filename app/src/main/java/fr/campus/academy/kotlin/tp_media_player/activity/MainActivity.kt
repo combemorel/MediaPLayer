7da5e446ec2fba59f2b47a1d586faeafab2bf42a
combemorel <d.combemorel@orange.fr>
2021-02-16 13:24:48 +0100

    ajout TODO

--- app/src/main/java/fr/campus/academy/kotlin/tp_media_player/activity/MainActivity.kt
+++ app/src/main/java/fr/campus/academy/kotlin/tp_media_player/activity/MainActivity.kt
@@ -9,4 +9,14 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+    // TODO: lecteur audio
+    // TODO: liste fichier audio sur appareil
+    // TODO: boutton ajoute favoris
+    // TODO: sauvegarde favoris
+    // TODO: button action bar favoris
+    // TODO: notification lors du lancement de la lecture
+    // TODO: notification lecture/pause (+ titre + dureé)
+    // Impossible pour android de ferme la lecture
+    // titre suivant auto (+ dernier = reload)
+
 }
